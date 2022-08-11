067743ab9ea81c3192c37c71026aa320b142ce4d
mewecoffee <jdorsey760@gmail.com>
2022-08-11 16:38:35 -0700

    Delete RemoveADComputer_list.ps1

--- RemoveADComputer_list.ps1
+++
@@ -1,7 +0,0 @@
-$TextFile = 'C:\users\jdorsey\Downloads\temp\dave_ad_computers.txt'
-$ListOfComputers = Get-Content $TextFile
-
-foreach ($Computer in $ListOfComputers) {
-
-   Get-ADComputer $Computer | Remove-ADObject -Recursive -Confirm:$false
-}
